95412c0b246488229487361a9f283736de12eaa7
tongtong23333 <59009831+DLighTer@users.noreply.github.com>
2020-02-12 15:51:08 +0800

    Update and rename php to msg.php

--- msg.php
+++ msg.php
@@ -1,40 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>php_msg_board</title>
- 
-      <style>
-         
-          #username{
-             margin: 0px 10px 0px 5px;
-             padding-right: 5px;
-             border-right: 2px solid darkgray;
-         }
-         #msg{
-             border: 2px solid darkgray;
-             width: 300px;
-             padding: 5px;
-         }
-     </style>
- </head>
- <body>
- 
- 
- 
- <form action="msg.php" method="POST" >
- 
- <textarea name="msg"></textarea>
- 
- <input type="submit" value="submit">
- 
- </form>
- 
- 
 <?php
- 
  $username="root";
- 
  $conn=mysqli_connect('localhost','root','herewego666','liuyanban');//分别代表对应数据库服务器地址、用户名、密码、所要操作的数据库名称//
  
  mysqli_query($conn,"set names utf8");
