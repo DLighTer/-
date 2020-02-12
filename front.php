00086972aaa051c3e653df43a67cc14b73d3b0f7
tongtong23333 <59009831+DLighTer@users.noreply.github.com>
2020-02-12 21:26:51 +0800

    Update front.php

--- front.php
+++ front.php
@@ -1,31 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 <meta charset="UTF-8">
 <title>php_msg_board</title>
- 
-      <style>
-         
-          #username{
-             margin: 0px 10px 0px 5px;
-             padding-right: 5px;
-             border-right: 2px solid darkgray;
-         }
-         #msg{
-             border: 2px solid darkgray;
-             width: 300px;
-             padding: 5px;
-         }
-     </style>
+
  </head>
+ 
+ 
+ 
  <body>
- 
- 
+
  
  <form action="msg.php" method="POST" >
  
- <textarea name="msg"></textarea>
- 
+ 用户名：<input type="text" name="id"> <br>
+ 留言内容：<textarea name="msg"></textarea>
  <input type="submit" value="submit">
  
  </form>
