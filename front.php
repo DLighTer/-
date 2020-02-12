76324751c20e7320b346628b41271d369cd7e4db
tongtong23333 <59009831+DLighTer@users.noreply.github.com>
2020-02-12 15:50:03 +0800

    Create front.php

---
+++ front.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>php_msg_board</title>
+ 
+      <style>
+         
+          #username{
+             margin: 0px 10px 0px 5px;
+             padding-right: 5px;
+             border-right: 2px solid darkgray;
+         }
+         #msg{
+             border: 2px solid darkgray;
+             width: 300px;
+             padding: 5px;
+         }
+     </style>
+ </head>
+ <body>
+ 
+ 
+ 
+ <form action="msg.php" method="POST" >
+ 
+ <textarea name="msg"></textarea>
+ 
+ <input type="submit" value="submit">
+ 
+ </form>
+ 
